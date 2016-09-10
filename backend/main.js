4d7bb3868b89b2ccffd8eed6f4e971fdcc24118e
teddyzhu <teddyzhu15@gmail.com>
2016-09-10 18:21:03 +0800

    tmp

--- backend/main.js
+++ backend/main.js
@@ -3,25 +3,24 @@ const {
     app,
     BrowserWindow,
     ipcMain
-} = electron
+    } = electron;
 let config = {
     dbname: 'mud.sqlite3',
-    defaultEncoding: 'gbk',
+    defaultEncoding: 'GBK',
     host: '112.126.83.105',
     port: 5555,
-    enter: '\r\n'
+    enter: '\n',
+    macroprefix: '!'
 };
-//#session wx 112.126.83.105 5555
 const fs = require('fs');
 const util = require('util');
 const encoding = require('encoding');
 const sqlite3 = require('sqlite3');
 const net = require('net');
 const ansi_up = require('ansi_up');
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-let connect
+
+let mainWindow;
+let connect;
 
 function createWindow() {
     // Create the browser window.
@@ -33,84 +32,106 @@ function createWindow() {
 
     // and load the index.html of the app.
     mainWindow.loadURL(`file://${__dirname}/../index.html`)
-
-    mainWindow.webContents.on('dom-ready', function() {
-        mainWindow.webContents.send('dom-ready-event');
+    mainWindow.webContents.on('connect-server', function () {
         connectServer();
     });
+    mainWindow.webContents.on('dom-ready', function () {
+        mainWindow.webContents.send('dom-ready-event');
+        bindMessage();
+    });
 
-    mainWindow.on('closed', function() {
+
+    mainWindow.on('closed', function () {
 
         mainWindow = null
     })
 }
 
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
+});
 
-app.on('activate', function() {
+app.on('activate', function () {
     if (mainWindow === null) {
         createWindow()
     }
-})
+});
 
+function disconnectServer() {
+    if (connect) {
+        connect.end();
+        connect = null;
+    }
+}
+
+function connectServer() {
+    disconnectServer();
+    connect = net.connect({
+        port: config.port,
+        host: config.host
+    }, function () {
+        mainWindow.webContents.send("content-update", {
+            result: true,
+            data: '已连接到[' + config.host + ':' + config.port + ']服务器！<br>'
+        });
+    });
+
+    connect.on('data', function (data) {
+        var result = encoding.convert(data, "utf-8", config.defaultEncoding).toString();
+
+        result = ansi_up.ansi_to_html(result.toString());
+
+        mainWindow.webContents.send('content-update', {
+            result: true,
+            data: result
+        });
+    });
+    connect.on('end', function () {
+        connect = null;
+        mainWindow.webContents.send('content-update', {
+            result: true,
+            data: '断开与服务器的连接'
+        });
+    });
+}
 function initDateBase() {
-    fs.exists(config.dbname, function(exists) {
+    fs.exists(config.dbname, function (exists) {
         if (exists) {
             // serve file
         } else {
             // mongodb
         }
     });
+
 }
 
-function connectServer() {
-    ipcMain.on("send-server-commad", function(event, arg) {
-        console.log('send-server-commad-recv');
-        console.log(arg);
+function bindMessage() {
+    ipcMain.on("send-server-commad", function (event, arg) {
         if (connect) {
             if (arg && arg.result) {
-                console.log('write data')
-                console.log(arg.data);
-                connect.write(arg.data + config.enter);
+                let encode = encoding.convert(arg.data + config.enter, config.defaultEncoding,"UTF-8");
+                console.log("ASD"+encode.toString());
+                connect.write(encode.toString() , config.defaultEncoding);
             }
         }
-    })
-    ipcMain.on("disconnect-server", function(event, arg) {
+    });
+    ipcMain.on("disconnect-server", function (event, arg) {
         if (connect) {
             if (arg && arg.result) {
                 connect.end();
             }
         }
-    })
+    });
 
-    connect = net.connect({
-        port: config.port,
-        host: config.host
-    }, function() {
-        console.log('连接到服务器！');
-    });
-    connect.on('data', function(data) {
-        var result = encoding.convert(data, "utf-8", config.defaultEncoding).toString();
-        console.log(result.toString());
-        result = ansi_up.ansi_to_html(result.toString());
+    connectServer();
+}
 
-        console.log('content-update-send');
-        mainWindow.webContents.send('content-update', {
-            result: true,
-            data: result
-        });
-    });
-    connect.on('end', function() {
-        mainWindow.webContents.send('content-update', {
-            result: true,
-            data: '断开与服务器的连接'
-        });
-    });
+
+function resloveMacro(cmd){
+
 }
--- render/renderer.js
+++ render/renderer.js
@@ -1,10 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+let cmdHistory = [], index = 0;
 const {
     ipcRenderer
-} = require('electron');
-ipcRenderer.on('content-update', function(event, arg) {
+    } = require('electron');
+ipcRenderer.on('content-update', function (event, arg) {
     console.log('content-update-recv')
     console.log(arg)
     if (arg && arg.result) {
@@ -13,8 +14,11 @@ ipcRenderer.on('content-update', function(event, arg) {
         that.scrollTop = that.scrollHeight;
     }
 });
-ipcRenderer.on('dom-ready-event', function() {
-    HTMLElement.prototype.appendHTML = function(html) {
+ipcRenderer.on('dom-ready-event', function () {
+    String.prototype.trim = function () {
+        return this.replace(/(^\s*)|(\s*$)/g, "");
+    };
+    HTMLElement.prototype.appendHTML = function (html) {
         var divTemp = document.createElement("div"),
             nodes = null,
             fragment = document.createDocumentFragment();
@@ -27,15 +31,58 @@ ipcRenderer.on('dom-ready-event', function() {
         nodes = null;
         fragment = null;
     };
-    document.getElementById('cmd').addEventListener('keydown', function(e) {
-        if (e.keyCode == 13) {
+    document.getElementById('cmd').addEventListener('keydown', function (e) {
+        resolveKeyDown(this, e.keyCode);
+    })
+});
+
+function resolveKeyDown(obj, code) {
+    switch (code) {
+        case 13:
+        {
+            let val = obj.value.trim();
+            if (!val) return;
+            cmdHistory.push(val);
+            index = cmdHistory.length ;
             ipcRenderer.send('send-server-commad', {
                 result: true,
-                data: this.value
+                data: val
             });
-            this.value ='';
+            obj.value = '';
+            break;
         }
-    })
-
-
-})
+        case 38:
+        {
+            //上
+            if (cmdHistory.length == 0) {
+                obj.value = '';
+                break;
+            }
+            index--;
+            index = index < 0 ? 0 : index;
+            obj.value = cmdHistory[index];
+            break;
+        }
+        case 40:
+        {
+            //下
+            if (cmdHistory.length == 0) {
+                obj.value = '';
+                break;
+            }
+            index++;
+            index = index > cmdHistory.length ? cmdHistory.length  : index;
+            obj.value = cmdHistory[index - 1];
+            break;
+        }
+        default:
+        {
+            return;
+        }
+    }
+}
+function connectServer() {
+    document.getElementById('content').innerHTML = "";
+    cmdHistory = [];
+    ipcRenderer.send("connect-server")
+}
